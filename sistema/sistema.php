b86c76734188732250c43d19da8e24fb6e452e7f
Lucas Nuernberg <lucasnogoncalves@hotmail.com>
2021-03-27 20:35:58 -0300

    Second commit

--- sistema/sistema.php
+++ sistema/sistema.php
@@ -18,6 +18,10 @@
             <button class="btn btn-light"> <a href="modCar.php"> Modificar carros</a></button>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+  
+    </script>
 </body>
 </html>
 
